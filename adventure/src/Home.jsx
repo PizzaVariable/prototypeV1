4cc50856743a071bc8c9cd361fbaff3f4215c999
Chase Graffeo <chasegraffeo@gmail.com>
2020-03-28 18:42:45 -0500

    10cards made

--- adventure/src/Home.jsx
+++ adventure/src/Home.jsx
@@ -1,16 +1,38 @@
 import * as React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
     
     class Home extends React.Component {
-         
+         state = {
+             username:""
+         }
+
+         handleUsernameChange = e => this.setState({ username: e.target.value});
+            handleSubmit = e => {
+                e.preventDefault();
+                this.props.handleNameSubmit( {
+                    username: this.state.username
+                });        
+                this.setState({
+                  username: ""
+                });
+              }
+            
+        
+
+
+
               render() {
                     return (
+                        <Container as="main"> 
+                        <Card className="mt-3 justify-content-center">
                           <Form className="form-group p-3 shadow-lrg">
                           <Form.Label>STORY NAME</Form.Label>
                           <Form.Control
-                            //value={this.state.username}
-                            //onChange={this.handleUsernameChange}
+                            value={this.state.username}
+                            onChange={this.handleUsernameChange}
                             placeholder="Charcter Name"
                             type="text"
                           />
@@ -21,9 +43,11 @@ import Button from "react-bootstrap/Button";
                           >
                     
                             <span className="mx-1"></span>
-        Chirp It!
+        Create Charcter..
       </Button>
     </Form>
+    </Card>
+    </Container>
     );
   }
 }
